a8b6fbb158d5ee35c26a74ffbbc2ef5975fbb8a6
Igor Spichkin <igorsa.ux@outlook.com>
2023-05-26 14:43:41 +0300

    fix and update dependencies

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 extern "C" fn _start_rust() {
     unsafe {
         let pci = Pci::new(0x30000000);
-        let mut tts = Tts::from(pci.device(1).unwrap());
+        let mut tts = Tts::from(pci.device(0).unwrap());
 
         tts.flush();
         tts.write_string("Hello, world!");
