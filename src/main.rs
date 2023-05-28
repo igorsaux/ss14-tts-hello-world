df0ae5198eaaf0cf22cd485f5f5b26a16f9941ca
Igor Spichkin <igorsa.ux@outlook.com>
2023-05-28 21:11:06 +0300

    update hello world

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,27 @@
 #![no_std]
 #![no_main]
-#![feature(panic_info_message, strict_provenance)]
 
-use core::arch::global_asm;
+use pci::Pci;
+use tts::Tts;
 
-use drivers_pci::Pci;
-use drivers_tts::Tts;
-
-global_asm!(include_str!("asm.S"));
-
-#[no_mangle]
-extern "C" fn eh_personality() {}
+use riscv_rt::entry;
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
-extern "C" fn _start_rust() {
+#[entry]
+fn main() -> ! {
     unsafe {
-        let pci = Pci::new(0x30000000);
+        let pci = Pci::default();
         let mut tts = Tts::from(pci.device(0).unwrap());
 
         tts.flush();
         tts.write_string("Hello, world!");
         tts.speech();
         tts.flush();
-    }
-}
 
-#[no_mangle]
-extern "C" fn _start_trap() {
-    loop {}
+        loop {}
+    }
 }
